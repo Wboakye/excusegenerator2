fc9cade0f3724f0c3c89295e3ffcfa64b680a543
wboakye <wboak001@fiu.edu>
2019-03-09 17:05:45 +0000

    edited

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function generateExcuse(){
     const art =['The', 'My', 'Your']
     const subject = ['mailman', 'teacher', 'bus driver', 'wife', 'president'];
-    const action = ['killed', 'threw', 'bit', 'hurt', 'broke'];
+    const action = ['killed', 'threw', 'bit', 'stole', 'broke'];
     const what = ['my dog', 'my feelings', 'my car', 'my foot', 'my house']
     const predicate = ['slowly', 'quickly', 'aggressively', 'menacingly', 'spitefully'];
  
@@ -9,6 +9,6 @@ function generateExcuse(){
 
     let excuse = art[Math.floor(Math.random() * 3)] + " " + subject[Math.floor(Math.random() * 5)] + " " + action[Math.floor(Math.random() * 5)] +" " + what[Math.floor(Math.random() * 5)] + " " + predicate[Math.floor(Math.random() * 5)];
     
-    document.getElementById("excuse").innerHTML = excuse;
+    document.querySelector("#excuse").innerHTML = excuse;
 
 }
